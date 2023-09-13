17db7cbfaf12982490801861dee011795af1ea67
Maxv1z <meitstudying@gmail.com>
2023-09-13 11:04:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='result-block'>
+        <div className="your-result left">
+          <h3>Your Result</h3>
+          <div className="result-round">
+            <div><h1>76</h1><p>of 100</p></div>
+          </div>
+
+          <h2>Great</h2>
+          <div className='bottom-text'>
+            <p>You scored higher than 65% of the people who have taken these tests.</p>
+          </div>
+
+        </div>
+
+        <div className="summary right">
+          <h1>Summary</h1>
+
+          <div className="summary-block">
+            Reaction 80 / 100
+          </div>
+
+          <div className="summary-block">
+            Memory
+            92 / 100
+          </div>
+
+          <div className="summary-block">
+            Verbal
+            61 / 100
+          </div>
+
+          <div className="summary-block">
+            Visual
+            72 / 100
+          </div>
+
+
+          <button>Continue</button>
+        </div>
+      </div>
+    </div >
   );
 }
 
